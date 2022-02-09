82f171fbd34a8a67bbd93d7de0fa930da05e42f2
TomH1775 <98777089+TomH1775@users.noreply.github.com>
2022-02-09 10:44:27 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,4 @@
+basic.forever(function () {
+    basic.showString("Tom")
+    basic.showString("Today the world is doomed everyone is running to the moon but the moon is too small so some people will go to Mars")
+})
